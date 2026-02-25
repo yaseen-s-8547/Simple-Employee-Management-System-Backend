1a1efa0fc1676f20e74c110b6f620e7b630c3edb
yaseen-8547 <ycs613678@gmail.com>
2026-02-25 20:11:51 +0530

    we set up post method and patch method

--- app.js
+++ app.js
@@ -1,20 +1,29 @@
 require("dotenv").config()
-const express=require("express")
-const cors=(require("cors"))
-const app=express()
+const express = require("express")
+const cors = (require("cors"))
+const app = express()
 app.use(cors())
 app.use(express.json())
-let employedetails=[]
-app.get('/employees',(req,res)=>{
+let employedetails = []
+app.get('/employees', (req, res) => {
     res.json(employedetails)
 })
-app.post('/items',(req,res)=>{
-    const{name}=req.body
-    const newItem={id:Date.now(),name}
-    employeedetails.push(newItem)
-    res.json(newItem)
+app.post('/employees', (req, res) => {
+    const { name } = req.body
+    const newEmployee = { id: Date.now(), name: name  }
+    employedetails.push(newEmployee)
+    res.json(newEmployee)
+
+})
+app.patch('/employees/:id', (req, res) => {
+    const  id  = Number(req.params.id)
+    const emp = employedetails.find(e => e.id === id)
+    if (!emp) return res.status(404).json({ message: "no employee found" })
+     Object.assign(emp, req.body)
+    res.json(emp)
+
 })
 
-app.listen(process.env.PORT,()=>{
+app.listen(process.env.PORT, () => {
     console.log("server running on port:", process.env.PORT)
 })
