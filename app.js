f481f8111b14b4d3274f882e31e5854b5ea1cd42
yaseen-8547 <ycs613678@gmail.com>
2026-03-05 13:18:56 +0530

    mongodb connected

--- app.js
+++ app.js
@@ -1,45 +1,50 @@
+import mongoose from "mongoose"
 require("dotenv").config()
 const express = require("express")
 const cors = (require("cors"))
 const app = express()
 app.use(cors())
 app.use(express.json())
-let employedetails = []
-app.get('https://simple-employee-management-system-u5c1.onrender.com/employees', (req, res) => {
+mongoose.connect("mongodb://localhost:27017/emsdb")
+const empSchema=new mongoose.Schema({name:String,role:String,Salary:Number,mailId:String,phone:Number})
+const Employees=mongoose.model("Employees",empSchema)
+app.get('/employees', async (req, res) => {
     const search =req.query.search
     if(!search){
-        return  res.json(employedetails)
+        const employees = await Employees.find()
+        return  res.json(employees)
     }
-    const filtereditem=employedetails.filter(det=>det.name?.toLowerCase().includes(search.toLowerCase())||det.id.toString().includes(search))
+    const filteredEmployees=await Employees.find({name:{$regx:search,$option:"i"}})
 
-    res.json(filtereditem)
+    res.json(filteredEmployees)
 })
-app.post('https://simple-employee-management-system-u5c1.onrender.com/employees', (req, res) => {
+app.post('/employees', async (req, res) => {
     const { name } = req.body
-    const newEmployee = { id: Date.now(), name: name  }
-    employedetails.push(newEmployee)
+    const newEmployee = await Employees.create({name})
+    
     res.json(newEmployee)
 
 })
-app.patch('https://simple-employee-management-system-u5c1.onrender.com/employees/:id', (req, res) => {
-    const  id  = Number(req.params.id)
-    const emp = employedetails.find(e => e.id === id)
-    if (!emp) return res.status(404).json({ message: "no employee found" })
-     Object.assign(emp, req.body)
-    res.json(emp)
+app.patch('/employees/:id',  async (req, res) => {
+    const  id  = req.params.id
+    const patchedItem =await Employees.findByIdAndUpdate(id,req.body,{new:true})
+    if(!patchedItem){
+        res.status(404).json({message:'not found'})
+    } 
+    res.json(patchedItem)
 
 })
-app.delete('https://simple-employee-management-system-u5c1.onrender.com/employees/:id',(req,res)=>{
-    const id=Number(req.params.id)
-    const originallength =employedetails.length
-    employedetails=employedetails.filter(det=>det.id!==id)
-    if(originallength===employedetails.length){
+app.delete('/employees/:id',async (req,res)=>{
+    const id=req.params.id
+    const deletedItem = await Employees.findByIdAndDelete(id)
+    if(!deletedItem){
         return res.status(404).json({message:"item not found "})
     }
+   
     res.json(204).send() 
 
 })
 const PORT = process.env.PORT || 5000
 app.listen(process.env.PORT, () => {
-    console.log("server running on port:", process.env.PORT)
+    console.log("server running on port:", PORT)
 })
